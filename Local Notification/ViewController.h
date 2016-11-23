f06304f11aa3cb9fe62eb0680fa135edc8eb27bc
Student-002 <Student-002@student-k-002.local>
2016-11-23 09:57:06 +0530

    Initial Commit

---
+++ Local Notification/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Local Notification
+//
+//  Created by Student-002 on 23/11/16.
+//  Copyright © 2016 Felix-its. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)firenotification:(id)sender;
+
+@end
+
---
+++ Local Notification/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Local Notification
+//
+//  Created by Student-002 on 23/11/16.
+//  Copyright © 2016 Felix-its. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)firenotification:(id)sender {
+    
+    
+    
+    
+}
+@end
